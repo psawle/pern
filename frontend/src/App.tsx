ab7546519015247f7657ef6a1704c0d3d1de2775
Prashant Sawle <psawle4@gmail.com>
2026-02-25 23:02:06 +0530

    first frontend commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind Working
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
