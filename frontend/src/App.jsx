b138799cb99c4de1368f630c44cb7c96e4e3aea7
Prashant Sawle <psawle4@gmail.com>
2026-05-23 22:49:53 +0530

    added redux setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useEffect, useState } from 'react'
 import './App.css'
+import instance from './api/axiosConfig'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData,setUserData] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  const getUser = async () => {
+    try {
+      const res = await instance.get("/users");
+    setUserData(res)
+    } catch (error) {
+      setError(error.message)
+    } finally{
+      setLoading(false)
+    }
+    
+  }
+
+   useEffect(() => {
+    getUser();
+  },[])
   return (
     <>
     <h1 className='text-3xl font-bold underline'>Helloworld</h1>
+    {loading && <p>Loading...</p>}
+    {error && <p>{error}</p>}
+    {userData.length > 0 ? userData.map((item) => (
+     <div className=''>
+      <h1>{item.name}</h1>
+     </div>
+    )) : "No user found"}
     </>
   )
 }
